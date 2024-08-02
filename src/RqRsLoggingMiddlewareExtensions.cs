1989bc1277d0fbb8c95f8dc104616d2943a6b861
Supawat Tanmanee <supawat.t@outlook.com>
2024-08-02 14:01:57 +0700

    Add RqRsLoggingMiddlewareExtensions class
    Introduce RqRsLoggingMiddlewareExtensions class in the DMNSN.AspNetCore.Middleware.RqRsLogging namespace. This class provides extension methods for adding request and response logging middleware to an ASP.NET Core application.
    
    New methods added:
    - UseRqRsLoggingMiddleware(IApplicationBuilder builder)
    - UseRqRsLoggingMiddleware(IApplicationBuilder builder, IConfiguration configuration)
    - UseRqRsLoggingMiddleware(IApplicationBuilder builder, RqRsLoggingMiddlewareOptions options)
    - UseRqRsSingleLoggingMiddleware(IApplicationBuilder builder)
    - UseRqRsSingleLoggingMiddleware(IApplicationBuilder builder, IConfiguration configuration)
    - UseRqRsSingleLoggingMiddleware(IApplicationBuilder builder, RqRsLoggingMiddlewareOptions options)
    
    Define DefaultSection constant for default configuration section path. Import Microsoft.AspNetCore.Builder and Microsoft.Extensions.Configuration namespaces.

---
+++ src/RqRsLoggingMiddlewareExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace DMNSN.AspNetCore.Middleware.RqRsLogging
+{
+    public static class RqRsLoggingMiddlewareExtensions
+    {
+        private const string DefaultSection = "DMNSN:Middlewares:RqRsLogging";
+        public static IApplicationBuilder UseRqRsLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RqRsLoggingMiddleware>(new RqRsLoggingMiddlewareOptions());
+        }
+        public static IApplicationBuilder UseRqRsLoggingMiddleware(this IApplicationBuilder builder, IConfiguration configuration)
+        {
+            // Attempt to read the configuration section
+            var options = configuration.GetSection(DefaultSection).Get<RqRsLoggingMiddlewareOptions>() ?? new RqRsLoggingMiddlewareOptions();
+            return builder.UseMiddleware<RqRsLoggingMiddleware>(options);
+        }
+        public static IApplicationBuilder UseRqRsLoggingMiddleware(this IApplicationBuilder builder, RqRsLoggingMiddlewareOptions options)
+        {
+            return builder.UseMiddleware<RqRsLoggingMiddleware>(options);
+        }
+
+        public static IApplicationBuilder UseRqRsSingleLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RqRsSingleLoggingMiddleware>(new RqRsLoggingMiddlewareOptions());
+        }
+        public static IApplicationBuilder UseRqRsSingleLoggingMiddleware(this IApplicationBuilder builder, IConfiguration configuration)
+        {
+            // Attempt to read the configuration section
+            var options = configuration.GetSection(DefaultSection).Get<RqRsLoggingMiddlewareOptions>() ?? new RqRsLoggingMiddlewareOptions();
+            return builder.UseMiddleware<RqRsSingleLoggingMiddleware>(options);
+        }
+        public static IApplicationBuilder UseRqRsSingleLoggingMiddleware(this IApplicationBuilder builder, RqRsLoggingMiddlewareOptions options)
+        {
+            return builder.UseMiddleware<RqRsSingleLoggingMiddleware>(options);
+        }
+    }
+}
